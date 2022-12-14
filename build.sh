a9831e7bedbd864b43fcf3685c3d193f2a7e3cc9
Mehmet Tekman <mtekman89@gmail.com>
2022-12-14 12:46:12 +0100

    added public site generator

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## pacman -S uglify-js python-rcssmin
+
+pub_dir="public"
+pub_html="$pub_dir/index.html"
+
+tmp_dir="_temp_"
+tmp_js="$tmp_dir/minmang.js"
+tmp_css="$tmp_dir/minmang.css"
+
+mkdir -p $pub_dir $tmp_dir;
+
+## Mangle and compress JS and CSS
+uglifyjs *.js --rename --no-annotations -c -m --validate --toplevel --ie -o $tmp_js
+sed -i -r "1s|^|<script>|" $tmp_js
+echo "</script>" >> $tmp_js
+
+echo "<style>" > $tmp_css
+python -c 'from rcssmin import cssmin; print(cssmin(open("style.css", "r").read()))' >> $tmp_css
+echo "</style>" >> $tmp_css
+
+## Create initial public HTML without CSS and JS
+cat index.html | grep -vP "(<script src=|<link rel=)" > $pub_html
+
+## Paste all temp CS and JS into public HTML
+sed -i "/\/head/e cat $tmp_css" $pub_html
+sed -i "/External Libraries/e cat $tmp_js" $pub_html
