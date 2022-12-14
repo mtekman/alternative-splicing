ce00bd4da5267ce073340889212c01c818c90041
Mehmet Tekman <mtekman89@gmail.com>
2022-12-14 14:06:23 +0100

    added favicon and restructured scripts

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@
 pub_dir="deploy"
 pub_html="$pub_dir/index.html"
 
-tmp_dir="_temp_"
+tmp_dir="temp"
 tmp_js="$tmp_dir/minmang.js"
 tmp_css="$tmp_dir/minmang.css"
 
 mkdir -p $pub_dir $tmp_dir;
 
 ## Mangle and compress JS and CSS
-uglifyjs *.js --rename --no-annotations -c -m --validate --toplevel --ie -o $tmp_js
+uglifyjs js/*.js --rename --no-annotations -c -m --validate --toplevel --ie -o $tmp_js
 sed -i -r "1s|^|<script>|" $tmp_js
 echo "</script>" >> $tmp_js
 
@@ -21,8 +21,13 @@ python -c 'from rcssmin import cssmin; print(cssmin(open("style.css", "r").read(
 echo "</style>" >> $tmp_css
 
 ## Create initial public HTML without CSS and JS
-cat index.html | grep -vP "(<script src=|<link rel=)" > $pub_html
+cat index.html | grep -vP "(<script src=|<link rel=\"stylesheet\")" > $pub_html
 
 ## Paste all temp CS and JS into public HTML
 sed -i "/\/head/e cat $tmp_css" $pub_html
 sed -i "/External Libraries/e cat $tmp_js" $pub_html
+
+## Copy over favicon info
+cp favicons/* $pub_dir
+
+rm -rf $tmp_dir
